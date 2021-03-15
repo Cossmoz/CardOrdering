d6c4d05f21dc6ee2e40e44e61c0734b95c2e36b4
Constantin Zamotaev <cossx@yahoo.com>
2021-03-15 21:38:50 +0400

    project added

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/va9pwu94br70xh96?svg=true)](https://ci.appveyor.com/project/Cossmoz61571/cardordering)
---
+++ src/test/java/ru/netology/domain/CardOrderingTests.java
@@ -0,0 +1,20 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardOrderingTests {
+    @Test
+    void shouldSubmitRequest() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Константин");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+
+
+    }
+}
